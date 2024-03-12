8b5bdd768b9cbdfc82470a2e57aef2414ccc7bb3
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2024-03-12 10:51:38 +0900

    "생성자 초기화 순서"

--- C++/KDT2/01-03.Class/01-03.Class.cpp
+++ C++/KDT2/01-03.Class/01-03.Class.cpp
@@ -11,8 +11,6 @@ int main()
 		// 실체화된 사례(instance)를 만든다
 		// An Object is an instance of a class (객체는 클래스의 인스턴스입니다)
 		FClass ClassInstnace = FClass();
-		FClass ClassInstnace__ = FClass();
-		FClass ClassInstnace___ = FClass();
 		FClass ClassInstnace2 = FClass(TEXT("Text"));
 		FClass ClassInstance3 = FClass(ClassInstnace2);
 
@@ -27,33 +25,39 @@ int main()
 		FClass* Class = new FClass;
 		FClass* Class2 = new FClass;
 
-		//주소전달
+		// 주소 전달
 		//Class = Class2;
 
 		*Class = *Class2;
+		//Class->operator=(*Class2);
+
 		delete Class;
 		delete Class2;
 	}
-	//STATIC
+
+	// static
 	{
-		FClass ClassInstnace = FClass();
+		FClass::StaticStructInClassMember.Value = 100;
+		FClass ClassInstnace_ = FClass();
 		FClass ClassInstnace__ = FClass();
 		FClass ClassInstnace___ = FClass();
 
-		FClass::StaticStructClassMember.Value = 200;
+		FClass::StaticStructInClassMember.Value = 200;
 		FClass::Test();
-		ClassInstnace__.Test();
+		ClassInstnace___.Test();
 
-	
+		FUtils::Func();
 	}
 	{
 		FClass* Class = nullptr;
-		//Hello 함수에서 this포인터 접근하는 코드가없기때문에
-		//문제가 발생하지않는다.
+		// Hello 함수 내부에서 this에 접근하는 코드가 없어서
+		// 문제가 발생하지 않는다.
 		Class->Hello();
+		Class->Test();
 	}
-	//Friend : 내가 대상을 친구로 생각하면
-	//내 private(protected)에 접근할 수 있다.
+
+	// Friend: 내가 대상을 친구로 생각하면
+	// 내 private(protected)에 접근할 수 있어
 	{
 		class FClassA
 		{
@@ -63,21 +67,31 @@ int main()
 			{
 
 			}
-			void SetValue(const int InValue) {
-				ClassAPrivateDataButCanAccessFromFirend = InValue;
+
+			void SetValue(const int InValue)
+			{
+				ClassAPrivateDataButCanAaccessFromFriend = InValue;
 			}
-			int ClassAPrivateDataButCanAccessFromFirend = 10;
+
+		private:
+			int ClassAPrivateDataButCanAaccessFromFriend = 10;
 		};
 
-		// 싱글턴(singletone)
+		// 생성자가 private이라 instance를 만들 수 없다!
+		/*FClassA Class;
+		Class.SetValue(100);*/
+
+		// 싱글턴(singleton) 패턴: instance가 하나만 있다
+		// UE: Subsystem
 		class FManagerClass
 		{
 		public:
 			static FManagerClass* Get()
 			{
-				static std::unique_ptr<FManagerClass> Instance{ new FManagerClass }; //= std::make_unique<FManagerClass>();
+				static std::unique_ptr<FManagerClass> Instance{ new FManagerClass };
 				return Instance.get();
 			}
+
 			~FManagerClass()
 			{
 			}
@@ -86,13 +100,17 @@ int main()
 			{
 				Class.SetValue(InValue);
 			}
-			
+
 		private:
 			FManagerClass()
 			{
+
 			}
+
+		private:
 			FClassA Class;
 		};
+
 		FManagerClass* Instance = FManagerClass::Get();
 		Instance->SetValue(1000);
 		Instance->SetValue(200);
@@ -102,74 +120,115 @@ int main()
 		FManagerClass::Get()->SetValue(200);
 		FManagerClass::Get()->SetValue(300);
 		//FManagerClass Test;	// 생성자가 private이라 instance를 만들 수 없다!
-
 	}
-	//이동 의미론(move semantics)
+
+	// 이동 의미론(move semantics)
 	{
-		//InValue(left value; 좌측값) 이름이 있는 변수(이름으로 주소를 얻을 수 있다)
-		//RValue(right value; 우측값) InValue가 아닌 나머지 리터럴(상수)임시객체
-		int a = 5 + 10;//a는 InValue 이름(a)을 가지고 있으며 &a를 통해 주소를 가져올 수 있다
-		//반면 5+10이라는 표현식은 RValue이다
-		//임시값이라서 해당 문장을 실행하고 나면 제거된다
-		//임시값의 복사본을 a라는 변수에 저장한다.
+		// lvalue(left value; 좌측값): 이름이 있는 변수(이름으로 주소를 얻어올 수 있다)
+		// rvalue(right vlaue; 우측값): lvalue가 아닌 나머지, 리터럴(상수), 임시 객체
+		int a = 5 + 10;	// a는 lvalue이며, 이름(a)을 갖고 있으며, &a로 주소를 가져올 수 있다
+		// 반면, 5+10 이라는 표현식은 rvalue다.
+		// rvalue는 임싯값이라서 해당 문장을 실행하고 나면 제거된다.
+		// 임싯값의 복사본을 a라는 변수에 저장하고 있습니다.
 
-
+		//			lvalue   rvalue
 		const char* Hello = "Hello";
-		std::string Message = std::string(Hello);
-							//InValue reference
-		auto Function = [](std::string& InString) {
-			InString;
+		std::string Message = std::string(Hello); // 둘다 lvalue
+
+		// lvalue reference
+		auto Function = [](std::string& InString)
+			{
+				InString;
 			};
 
-							//RValue reference
-		auto Function2 = [](std::string&& InString) {
-			InString[0] = 'A';
-			};
 
+		std::string* StringPointer = nullptr;
+		// rvalue reference
+		auto Function2 = [&StringPointer](std::string&& InString)
+			{
+				InString[0] = 'A';
+				StringPointer = &InString;
+			};
 		Function(Message);
-		Function2("WoW");
+		Function2("Wow");
 
 		std::string Message2 = "World!";
-		Function2(Message+ Message2);
-		
+		Function2(Message + Message2);
+
+		// rvalue가 지정된 함수를 벗어나는 순간 소멸자가 호출됨.
+		// 아래처럼 사용하면 동작을 보장할 수 없다
+		/*struct FTest
+		{
+			~FTest()
+			{
+				std::cout << "Test\n";
+				Value = 1;
+			}
+
+			int Value = 10;
+		};
+		FTest* Test = nullptr;
+		auto Function3 = [&Test](FTest&& InString)
+			{
+				Test = &InString;
+			};
+
+		Function3(FTest());
+
+		int abb = 0;*/
 	}
 
 	{
-		FClass Class = FClass();
+		FClass Class = FClass(TEXT("Wowowowow"));
+		//FClass ClassCopy = FClass(Class);
 		FClass ClassCopy = Class;
 
-		//std::move : Invalue를 rvalue로 cast한다
-		//이동생성자가 구현되어 있지않다면 복사 생성자가 호출된다.
-
+		// std::move : lvalue를 rvalue로 cast 한다
+		// 이동생성자가 구현되어 있지 않다면 복사 생성자가 호출됩니다.
 		FClass ClassMove = FClass(std::move(Class));
 	}
 	{
-		std::vector<int>Vector;
-		Vector.resize(100000);
+		std::vector<int> Vector;
+		Vector.resize(10000);
 		Vector[0] = 100;
 		Vector[1] = 2200;
 
 		std::vector<int> Vector2 = Vector;
-		Vector[0] = 200;
+		Vector[0] = 1000;
 		Vector2[0] = 999;
 
 		std::vector<int> Vector3 = std::move(Vector);
-		//Vector[0] = 999;
 		Vector3[2] = 1234;
-
 	}
 	{
 		std::vector<FClass> Vector;
-		Vector.resize(10);
+		Vector.resize(10);		// 공간 할당과 동시에 값을 초기화 한다
 
-		std::vector<FClass> Vecotr2 = Vector;
+		std::vector<FClass> Vector2 = Vector;
 		std::vector<FClass> Vector3 = std::move(Vector);
 	}
 	{
 		std::vector<FClass> Vector;
-		Vector.reserve(10);
+		Vector.reserve(10);		// 미리 공간만 할당 한다
 		Vector.emplace_back();
 		Vector.emplace_back();
 		Vector.emplace_back();
+		Vector.emplace_back();
+		Vector.emplace_back();
+		Vector.emplace_back();
+		Vector.emplace_back();
+		Vector.emplace_back();
+		Vector.emplace_back();
+		Vector.emplace_back();
+
+		Vector.emplace_back();
+		Vector.emplace_back();
+		Vector.emplace_back();
+		Vector.emplace_back();
+		Vector.emplace_back();
+
+		Vector.emplace_back();
+		size_t S = Vector.size();
+		size_t C = Vector.capacity();
 	}
 }
--- C++/KDT2/01-03.Class/Class.h
+++ C++/KDT2/01-03.Class/Class.h
@@ -1,8 +1,9 @@
 #pragma once
 #include <iostream>
 #include <string>
+#include <vector>
 #include <Windows.h>
-#include<vector>
+
 // Class, Struct
 //{
 //	// Members...
@@ -43,33 +44,38 @@ struct FStruct
 
 class FClass
 {
-public:
-	//프로그램이 켜질때 해당 인스턴스가 만들어진다.
-	//꺼질때 삭제
-	//static 변수에 inline키워드를 붙히면
-	//h에 선언만해도 해당 인스턴스를 만들어준다
-	//하지만 dll에서 이걸 사용할때 아주 주의해서 사용해야 한다.
-	inline static FStruct StaticStructClassMember;
-
-	//static 함수를 만들 수있다!
-	//this Pointer가 파라미터로 들어오지 않는 함수
-	//일반 함수와 동일
-	static void Test() {
-		//Value;
-	}
-	//thiscall의 특성을 생각하면
-	//this pointer에 접근하지 않으면 문제가 없어야한다
-
-	void Hello() {
-		//*Pointer = 10800;
-	}
 	// 기본 접근 지정자: private
+public:
+	// 프로그램이 켜질 때 해당 인스턴스가 만들어집니다.
+	// 꺼질때 삭제됩니다.
+	// static 변수에 inline키워드를 붙히면
+	// h에 선언만 해도 인스턴스를 만들어 줍니다.
+	// 하지만 dll에서 이걸 사용할떄 아주 주의해서 사용해야 합니다.
+	inline static FStruct StaticStructInClassMember;
+
+	// static 함수를 만들수 있다!
+	// this pointer가 파라미터로 들어오지 않는 함수
+	// 일반 함수와 동일
+	static void Test()
+	{
+		std::cout << "Test\n";
+		/*Value;
+		this;*/
+	}
+
+	// __thiscall의 특성을 생각하시면
+	// this pointer에 접근하지 않으면 문제가 없어야 한다
+	void Hello()
+	{
+		std::cout << "Hello\n";
+		//*Pointer = 10999;
+	}
+
 public:
 	FClass()
 		: Value(100)
+		, Pointer(new int(Value))
 	{
-		Pointer = new int{ Value };
-
 		// 함수 내부에 있는 static 변수의 경우
 		// 해당 함수의 해당 위치에 처음 왔을때 인스턴스가 만들어지며
 		// 이후에는 만들어지지 않고, 해당 메모리에 계속 접근은 할수 있다
@@ -80,9 +86,9 @@ public:
 	}
 
 	FClass(const std::wstring_view InString)
-		: String(InString)
+		: Pointer{ new int(Value) }
+		, String(InString)
 	{
-		Pointer = new int{ Value };
 		std::wcout << String << std::endl;
 	}
 
@@ -91,28 +97,30 @@ public:
 	// 아래처럼 추가로 동적할당에 대한 처리를 하는 경우 깊은 복사
 	FClass(const FClass& InOther)
 		: Value(InOther.Value)
-		, String(InOther.String)
 		, Pointer(new int{ Value })
+		, String(InOther.String)
 	{
 		std::wcout << String << std::endl;
 	}
 
-	//이동 생성자
+	// 이동 생성자
 	FClass(FClass&& InOther) noexcept
-		//String에 구현되어 있는 이동생성자에 의해서
-		//this->String이 생성된다.
+		// string에 구현되어 있는 이동 생성자에 의해서
+		// this->String이 생성됩니다.
 
-		//string은 특정 상황을 만족할때 데이터를 stack에 보관
-		//이 상황일 경우 복사가 move를 한다고 하더라도 발생
-		//이 경우에는 move가 의미가 없다.
-		//하지만 문자열이 긴 경우 Heap에 할당
-		//이경우 주소만 변경하면 된다.(성능향상)
-		:Value(InOther.Value)
-		,String(std::move(InOther.String))
-		,Pointer(InOther.Pointer)
+		// string은 특정 상황을 만족했을때 데이터를 stack 보관
+		// 이 상황일 경우 복사가 move를 한다고 하더라도 발생
+		// 이 경우에는 move가 의미가 없다
+		// 하지만 문자열이 긴 경우 Heap에 할당되는데
+		// 이럴때는 주소만 변경하면 되기 때문에 빠르다
+		: Value(InOther.Value)
+		, Pointer(InOther.Pointer)
+		, String(std::move(InOther.String))
 	{
+		InOther.Value = 0;
 		InOther.Pointer = nullptr;
 	}
+
 	~FClass()
 	{
 		if (Pointer)
@@ -124,8 +132,8 @@ public:
 
 	// 대입 연산자 오버로딩(선언하지 않더라도 기본적으로 만들어줌)
 	// 기본 대입 연산자로 복사하는 경우 이를 얕은 복사
-	//void operator=(const FClass& InOther);
-
+	// 아래처럼 추가로 포인터 값복사 처리를 하는 경우 깊은 복사
+	//void operator=(const FClass& InOther)
 	FClass& operator=(const FClass& InOther)
 		//FClass* operator=(const FClass& InOther)
 	{
@@ -141,18 +149,15 @@ public:
 		Value = InOther;
 		return *this;
 	}
-	void SetValue(int a) {
-		this->Value = a;
-	}
+
 private:
 	int Value = 0;
 	int* Pointer = nullptr;
 	std::wstring String;
 };
 
-class FUtils {
+class FUtils
+{
 public:
-	static void Func() {
-
-	}
+	static void Func() {}
 };
