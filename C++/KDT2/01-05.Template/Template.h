9345661757f44e14e2a6eaeb55e1e8bc59e2e3c9
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2024-03-13 15:02:46 +0900

    "루프 언롤링"

--- C++/KDT2/01-05.Template/Template.h
+++ C++/KDT2/01-05.Template/Template.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <iostream>
-
+#include <format>
 inline int Sum(const int a, const int b)
 {
 	return a + b;
@@ -17,6 +17,14 @@ template<class T>
 T TemplateSum(const T& a, const T& b)
 {
 	return a + b;
+}
+
+//템플릿 특수화
+//특정 타입에 대해서는 해당 버젼의 Template 함수를 호출
+template<>
+inline double TemplateSum(const double &InA,const double &InB) {
+	std::cout << "Double 버전의 Template Sum\n";
+	return InA + InB;
 }
 
 template<class T1, class T2, class T3 = double>
@@ -92,3 +100,66 @@ inline FStruct operator+(const FStruct& InA, const FStruct& InB) {
 	temp.Value2 = InA.Value2 + InB.Value2;
 	return temp;
 }
+inline void HandleValue(int InValue) {
+	std::cout << "int : " << InValue << std::endl;
+}
+inline void HandleValue(float InValue) {
+	std::cout << "float : " << InValue << std::endl;
+}
+
+inline void HandleValue(std::string_view InValue) {
+	std::cout << "string_view : " << InValue << std::endl;
+}
+
+template<typename...T>
+void ProcessValue(const T&... InValues) {
+	(HandleValue(InValues), ...);
+}
+
+// consteval 키워드를 붙히면 컴파일 타임에 함수호출 결과를 받아올 수 있습니다.
+// 컴파일 타임에 값을 예측할 수 있는 경우 계산해서 바로 값을 넣어버린다.
+consteval inline unsigned long long Factorial(unsigned char f)
+{
+	if (f == 0)
+	{
+		return 1;
+	}
+	else
+	{
+		return f * Factorial(f - 1);
+	}
+}
+
+template <unsigned char f>
+class FactorialTemplate
+{
+public:
+	static const unsigned long long Value{ f * FactorialTemplate<f - 1>::Value };
+};
+
+template<>
+class FactorialTemplate<0>
+{
+public:
+	static const unsigned long long Value{ 1 };
+};
+
+template<int i>
+class TLoop
+{
+public:
+	template<typename FuncType>
+	static inline void Run(FuncType Function)
+	{
+		TLoop<i - 1>::Run(Function);
+		Function(i);
+	}
+};
+
+template<>
+class TLoop<0>
+{
+public:
+	template<typename FuncType>
+	static inline void Run(FuncType) {}
+};
