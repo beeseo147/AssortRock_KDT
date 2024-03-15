eb71ff82db61e54a5e85c5804e4a407b1dad7af8
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2024-03-15 17:24:21 +0900

    "Vector"

--- C++/KDT2/01-07.Vector/01-07.Vector.cpp
+++ C++/KDT2/01-07.Vector/01-07.Vector.cpp
@@ -3,6 +3,8 @@
 
 #include <iostream>
 #include <vector>
+#include <format>
+#include <map>
 #include "Containers/Vector.h"
 
 using std::cout;
@@ -49,13 +51,20 @@ int main()
 			Vector.push_back(i);
 		}
 
+		auto ItInt = Vector.begin();
+		int Val = *ItInt;
+
 		KDT::vector<FStruct> Vector2;
 		//Vector2.reserve(10);
 		for (int i = 0; i < 10; ++i)
 		{
 			Vector2.push_back(FStruct(i));
+			//FStruct Test = FStruct(i);
+			//Vector2.push_back(Test);
 		}
-		Vector2.resize(11);
+		Vector2.resize(2);
+		Vector2.resize(1);
+		Vector2.resize(2);
 		Vector2.clear();
 		for (int i = 0; i < 5; ++i)
 		{
@@ -66,8 +75,25 @@ int main()
 		size_t Cap = Vector2.capacity();
 
 		FStruct& Data = Vector2[0];
+		KDT::vector<FStruct>::iterator It = Vector2.begin();
+		KDT::vector<FStruct>::iterator It2 = ++It;
+		KDT::vector<FStruct>::iterator It3 = It++;
+		++It;
+		--It;
+		(*It).Value;
+		It->Value;
+		//It3 = It;
+		KDT::vector<FStruct>::iterator It4 = It;
+		KDT::vector<FStruct>::iterator ItEnd = Vector2.end();
+
+		for (FStruct& Value : Vector2)
+		{
+			cout << std::format("Value: {}\n", Value.Value);
+		}
+
 		Data.Value = 89;
 		FStruct& Data2 = Vector2[1];
+		//FStruct& Data3 = Vector2["2"];
 		Data2.Value = 999;
 	}
 	{
@@ -101,5 +127,53 @@ int main()
 		++It;
 		++It;
 		--It;
+	}
+
+	// vector 사용시 주의점
+	{
+		std::vector<int> Vector;
+		Vector.push_back(10);
+		// 뎅글링 포인터
+		std::vector<int>::iterator It = Vector.begin();
+		int& Value = Vector.back();
+
+		// 재할당으로 인해 기존에 가리키던 주소가 무효화됨
+		// Value, It가 의미없는 주소를 가리킴
+		Vector.push_back(20);
+		std::vector<int>::iterator It2 = Vector.begin();
+		Vector.erase(Vector.begin());
+		/*for (int i = 0; i < 10; ++i)
+		{
+			if (...)
+			{
+				Vector.erase(Vector.begin() + i);
+			}
+		}*/
+	}
+	{
+		KDT::vector<int> Vector;
+		Vector.push_back(10);
+		// 뎅글링 포인터
+		KDT::vector<int>::iterator It = Vector.begin();
+		int& Value = Vector[0];
+		Vector.push_back(20);
+
+		// 우리 vector는 초기 size가 2로 잡혀서 2개 추가하면 그렇게됨
+		// 재할당으로 인해 기존에 가리키던 주소가 무효화됨
+		// Value, It가 의미없는 주소를 가리킴
+		Vector.push_back(30);
+	}
+	{
+		std::map<int, int> Map;
+		std::pair<std::map<int, int>::iterator, bool> It = Map.insert(std::make_pair(0, 100));
+		It.second; // insert 성공 유무
+		It.first->first;	// key int
+		It.first->second;	// value int
+		std::pair<std::map<int, int>::iterator, bool> It2 = Map.insert(std::make_pair(1, 100));
+
+		for (int i = 0; i < 10000; ++i)
+		{
+			Map.insert(std::make_pair(i, 100));
+		}
 	}
 }
--- C++/KDT2/01-07.Vector/Containers/Vector.h
+++ C++/KDT2/01-07.Vector/Containers/Vector.h
@@ -7,8 +7,83 @@ namespace KDT
 	}
 
 	template<class T>
+	class FVectorIterator
+	{
+	public:
+		FVectorIterator() = default;
+		FVectorIterator(const FVectorIterator&) = default;
+		FVectorIterator(T* InPointer) : Pointer(InPointer) {}
+		FVectorIterator& operator=(const FVectorIterator&) = default;
+
+		bool operator==(const FVectorIterator& InIter)
+		{
+			return Pointer == InIter.Pointer;
+		}
+		bool operator!=(const FVectorIterator& InIter)
+		{
+			return Pointer != InIter.Pointer;
+		}
+
+		// 선행 증가
+		FVectorIterator& operator++()
+		{
+			if (Pointer)
+			{
+				/*Pointer = Pointer + 1;
+				Pointer += 1;*/
+				++Pointer;
+			}
+			return *this;
+		}
+
+		// 후행 증가
+		FVectorIterator operator++(int)
+		{
+			FVectorIterator Temp = *this;
+			FVectorIterator::operator++();
+			return Temp;
+		}
+
+		// 선행 감소
+		FVectorIterator& operator--()
+		{
+			if (Pointer)
+			{
+				--Pointer;
+			}
+			return *this;
+		}
+
+		T& operator*() const
+		{
+			return *Pointer;
+		}
+
+		T* operator->() const
+		{
+			return Pointer;
+		}
+
+	private:
+		T* Pointer = nullptr;
+	};
+
+	template<class T>
 	class vector
 	{
+	public:
+		using iterator = FVectorIterator<T>;
+
+		_NODISCARD _CONSTEXPR20 iterator begin() noexcept
+		{
+			return iterator(Array);
+		}
+
+		_NODISCARD _CONSTEXPR20 iterator end() noexcept
+		{
+			return iterator(Array + Size);
+		}
+
 	public:
 		void push_back(const T& InData)
 		{
@@ -35,9 +110,10 @@ namespace KDT
 
 		void reserve(const size_t InSize)
 		{
-			const static size_t MinSize = 2;
+			const static size_t MinSize = 1;
 			// end 위치가 될 공간을 위해 + 1 을 해줬다
 			const size_t NewSize = std::max(MinSize, InSize) + 1;
+			const size_t NewCapacity = NewSize - 1;
 
 			//T* NewArray = new T[NewSize];
 			T* NewArray = (T*)malloc(sizeof(T) * NewSize);
@@ -45,13 +121,18 @@ namespace KDT
 			// 배열이 비어 있지 않은 경우
 			if (!empty())
 			{
+				const size_t OriginSize = Size;
+				Size = Size > NewCapacity ? NewCapacity : Size;
 				// 원래 들어있던 데이터를 복사해온다
-				for (int i = 0; i < Size; ++i)
+				for (size_t i = 0; i < Size; ++i)
 				{
 					//NewArray[i] = std::move(Array[i]);
 					// placement new: 이미 할당된 메모리 공간에 생성자를 호출
 					new(NewArray + i)T(std::move(Array[i]));
 					// new(&NewArray[i])
+				}
+				for (size_t i = 0; i < OriginSize; ++i)
+				{
 					Array[i].~T();
 				}
 			}
@@ -66,13 +147,20 @@ namespace KDT
 
 			// 신규 할당된 NewArray의 주소를 Array에 전달한다
 			Array = NewArray;
-			Capacity = NewSize - 1;
+			Capacity = NewCapacity;
 		}
 
 		void resize(const size_t InSize)
 		{
+			if (InSize == 0)
+			{
+				clear();
+				return;
+			}
+
+			const size_t OriginSize = Size;
 			reserve(InSize);
-			for (size_t i = Size; i < InSize; ++i)
+			for (size_t i = OriginSize; i < InSize; ++i)
 			{
 				push_back({});
 			}
@@ -107,6 +195,10 @@ namespace KDT
 		{
 			return Array[InIndex];
 		}
+		/*_NODISCARD T& operator[](std::string_view InIndex) const
+		{
+			return Array[std::atoi(InIndex.data())];
+		}*/
 
 	public:
 		vector() = default;
