36796280deecc26b22437ddc64172d43cd43eced
beeseo147 <87863897+beeseo147@users.noreply.github.com>
2024-03-20 15:34:13 +0900

    UserManagement

---
+++ C++/KDT2/02.UserManagementProgram/02.UserManagementProgram.cpp
@@ -0,0 +1,26 @@
+﻿// 02.UserManagementProgram.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+#include "MISC/Headers.h"
+#include "Classes/Account.h"
+#include "Classes/DataBase.h"
+
+// C++을 사용해서 간단한 
+// - 회원가입, 회원 탈퇴
+// - 로그인, 로그아웃
+// - 접속중인 유저 표시
+// - 입력 인터페이스
+
+// 회원 정보 관리
+// - class FDataBase
+// 계정 정보
+// - struct FAccount
+
+int main()
+{
+	/*FAccount Account("Hello", "World");
+
+	FAccount* NewAccount = GDataBase.CreateAccount(Account);
+	GDataBase.DeleteAccount(*NewAccount);
+	GDataBase.DeleteAccount(Account);
+	FAccount* NewAccount2 = GDataBase.CreateAccount(Account);*/
+}
---
+++ C++/KDT2/02.UserManagementProgram/MISC/Headers.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+#include <cassert>
+#include <format>
+#include <fstream>
+
+#include "rapidjson/document.h"
+#include "rapidjson/writer.h"
+#include "rapidjson/stringbuffer.h"
+
+using namespace std;
+using FAccountName = string;
